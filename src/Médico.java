22557e8ed31d71335a0fe667c992695fb1b26e19
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-17 21:18:37 -0300

    Criando arquivos aula Java

---
+++ src/Médico.java
@@ -0,0 +1,13 @@
+public class Médico {
+//Atributos da Classe
+// "tipo do atributo" "atributo"
+
+String Nome;
+String Faculdade;
+String Especialidade;
+String AreaDeAtuacao;
+int Crm;
+double CargaHorariaPlantao;
+double salario;
+
+}
